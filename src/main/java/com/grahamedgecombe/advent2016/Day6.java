6bf265032e5bc6f03eb8441b44b24e48c82b4b64
Graham Edgecombe <gpe@grahamedgecombe.com>
2016-12-06 19:26:59 +0000

    Add day 6 solution

---
+++ src/main/java/com/grahamedgecombe/advent2016/Day6.java
@@ -0,0 +1,50 @@
+package com.grahamedgecombe.advent2016;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.function.BiPredicate;
+
+public final class Day6 {
+	public static void main(String[] args) throws IOException {
+		List<String> messages = AdventUtils.readLines("day6.txt");
+		System.out.println(getMessage(messages, (a, b) -> a > b));
+		System.out.println(getMessage(messages, (a, b) -> a < b));
+	}
+
+	public static String getMessage(List<String> messages, BiPredicate<Integer, Integer> greaterThan) {
+		int len = messages.get(0).length();
+		int[][] letterFrequencies = new int[len][26];
+
+		for (String message : messages) {
+			for (int i = 0; i < message.length(); i++) {
+				char ch = message.charAt(i);
+				letterFrequencies[i][ch - 'a']++;
+			}
+		}
+
+		char[] message = new char[len];
+		for (int i = 0; i < len; i++) {
+			message[i] = mostFrequentLetter(letterFrequencies[i], greaterThan);
+		}
+
+		return new String(message);
+	}
+
+	private static char mostFrequentLetter(int[] frequencies, BiPredicate<Integer, Integer> greaterThan) {
+		char letter = 'a';
+		int frequency = frequencies[0];
+
+		for (int i = 1; i < frequencies.length; i++) {
+			if (frequencies[i] != 0 && greaterThan.test(frequencies[i], frequency)) {
+				letter = (char) ('a' + i);
+				frequency = frequencies[i];
+			}
+		}
+
+		return letter;
+	}
+
+	private Day6() {
+		/* empty */
+	}
+}
---
+++ src/test/java/com/grahamedgecombe/advent2016/Day6Test.java
@@ -0,0 +1,39 @@
+package com.grahamedgecombe.advent2016;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public final class Day6Test {
+	private static final List<String> messages = Arrays.asList(
+		"eedadn",
+		"drvtee",
+		"eandsr",
+		"raavrd",
+		"atevrs",
+		"tsrnev",
+		"sdttsa",
+		"rasrtv",
+		"nssdts",
+		"ntnada",
+		"svetve",
+		"tesnvt",
+		"vntsnd",
+		"vrdear",
+		"dvrsen",
+		"enarar"
+	);
+
+	@Test
+	public void testPart1() {
+		assertEquals("easter", Day6.getMessage(messages, (a, b) -> a > b));
+	}
+
+	@Test
+	public void testPart2() {
+		assertEquals("advent", Day6.getMessage(messages, (a, b) -> a < b));
+	}
+}
